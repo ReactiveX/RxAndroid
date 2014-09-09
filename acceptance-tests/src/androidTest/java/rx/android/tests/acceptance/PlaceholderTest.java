9613f19c0a79d4caec9c77757a1ddd91eac8d2d0
Matthias Kaeppler <m.kaeppler@gmail.com>
2014-09-09 16:29:13 +0200

    Add a sub-project for Android/RxJava acceptance tests

---
+++ acceptance-tests/src/androidTest/java/rx/android/tests/acceptance/PlaceholderTest.java
@@ -0,0 +1,11 @@
+package rx.android.tests.acceptance;
+
+import android.test.InstrumentationTestCase;
+
+public class PlaceholderTest extends InstrumentationTestCase {
+
+    public void testTruth() {
+        assertTrue(true);
+    }
+
+}
