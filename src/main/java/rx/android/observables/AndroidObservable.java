c11e0c2ad8482437b4360b966e75c3223709a573
Yaroslav Heriatovych <yaroslav@attendify.com>
2014-10-10 18:02:06 +0300

    Add fromSharedPreferencesChanges Observable
    this useful to listen for SharedPreferences changes

--- src/main/java/rx/android/observables/AndroidObservable.java
+++ src/main/java/rx/android/observables/AndroidObservable.java
@@ -17,6 +17,7 @@ package rx.android.observables;
 
 import static rx.android.schedulers.AndroidSchedulers.mainThread;
 
+import android.content.SharedPreferences;
 import rx.Observable;
 import rx.functions.Func1;
 import rx.operators.OperatorBroadcastRegister;
@@ -30,6 +31,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Handler;
+import rx.operators.OperatorSharedPreferenceChange;
 
 
 public final class AndroidObservable {
@@ -149,5 +151,14 @@ public final class AndroidObservable {
      */
     public static Observable<Intent> fromLocalBroadcast(Context context, IntentFilter filter){
         return Observable.create(new OperatorLocalBroadcastRegister(context, filter));
+    }
+
+    /**
+     * Create Observable that emits String keys whenever it changes in provided SharedPreferences
+     *
+     * Items will be observed on the main Android UI thread
+     */
+    public static Observable<String> fromSharedPreferencesChanges(SharedPreferences sharedPreferences){
+        return Observable.create(new OperatorSharedPreferenceChange(sharedPreferences));
     }
 }
