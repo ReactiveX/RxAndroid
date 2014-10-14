7c1a72b1525c3f99fde6f9d1cc76f8f7c43c9846
Dorvaryn <benjamin@odai.homelinux.net>
2014-10-14 11:43:04 +0100

    Adding some documentation

--- project/src/main/java/rx/android/observables/ReactiveDialog.java
+++ project/src/main/java/rx/android/observables/ReactiveDialog.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2014 Novoda, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package rx.android.observables;
 
 import android.app.DialogFragment;
@@ -14,6 +29,12 @@ import rx.Subscription;
 import rx.android.exception.CancelledException;
 import rx.resumable.SubscriberVault;
 
+/**
+ * A Wrapper for DialogFragment that allows to observe on the result of the User Interaction.
+ *
+ * @param <T> The type of data expected as return value from the fragment, can be boolean for confirmation dialogs,
+ *            or more complex for data input dialogs
+ */
 public class ReactiveDialog<T> extends DialogFragment {
 
     private static final String REACTIVE_DIALOG_KEY = "REACTIVE_DIALOG_KEY";
@@ -26,6 +47,11 @@ public class ReactiveDialog<T> extends DialogFragment {
         void onCancel();
     }
 
+    /**
+     *
+     * @param manager The android Fragment manager
+     * @return An observable on the User Interaction
+     */
     public Observable<T> show(final FragmentManager manager) {
         return Observable.create(new Observable.OnSubscribe<T>() {
             @Override
@@ -54,6 +80,10 @@ public class ReactiveDialog<T> extends DialogFragment {
         getListener().onCancel();
     }
 
+    /**
+     * Get the wrapped subscriber for the observable
+     * @return a listener that is an extended version of the subscriber
+     */
     protected ReactiveDialogListener<T> getListener() {
         Subscriber<Object> subscriber = subscriberVault.get(getSubscriberKey());
         if (subscriber == null) {
@@ -73,6 +103,12 @@ public class ReactiveDialog<T> extends DialogFragment {
         return (UUID) getArguments().getSerializable(REACTIVE_DIALOG_KEY);
     }
 
+    /**
+     * A wrapper for the subscriber from the observable.
+     * The wrapper add specialised failures such as CancelledException and removes itself from the vault upon completion or failure.
+     * @param <T> The type of data expected as return value from the fragment, can be boolean for confirmation dialogs,
+     *            or more complex for data input dialogs
+     */
     private class ReactiveDialogObserver<T> implements ReactiveDialogListener<T> {
 
         private final Subscriber<? super T> subscriber;
