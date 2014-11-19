bf15bd47e88748a68d8da92dc426b50e50087442
Matthias Kaeppler <m.kaeppler@gmail.com>
2014-11-19 17:58:13 +0100

    Remove JavaDoc from sample service

--- sample-app/src/main/java/rx/android/samples/eventbus/EventGeneratorService.java
+++ sample-app/src/main/java/rx/android/samples/eventbus/EventGeneratorService.java
@@ -7,13 +7,6 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.os.SystemClock;
 
-/**
- * An {@link IntentService} subclass for handling asynchronous task requests in
- * a service on a separate handler thread.
- * <p>
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
- */
 public class EventGeneratorService extends IntentService {
 
     public EventGeneratorService() {
