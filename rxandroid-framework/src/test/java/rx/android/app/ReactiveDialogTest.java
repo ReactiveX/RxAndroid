60bc8cee36c2562d3a8ee0fd266927b938c47ce8
Dorvaryn <ben@novoda.com>
2014-11-26 11:01:27 +0000

    Updating packages and headers to match new convention
    Headers have changed and packages convention as well.
    This is making sure the rxandroid-framework stays in line with those new
    conventions.

--- rxandroid-framework/src/test/java/rx/android/app/ReactiveDialogTest.java
+++ rxandroid-framework/src/test/java/rx/android/app/ReactiveDialogTest.java
@@ -1,11 +1,9 @@
 /**
- * Copyright 2014 Novoda, Inc.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package rx.android.observables;
+package rx.android.app;
 
 import android.app.Activity;
 import android.app.FragmentManager;
@@ -38,7 +36,7 @@ import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
-import static rx.android.observables.ReactiveDialog.Result;
+import static rx.android.app.ReactiveDialog.Result;
 
 @RunWith(RobolectricTestRunner.class)
 public class ReactiveDialogTest {
