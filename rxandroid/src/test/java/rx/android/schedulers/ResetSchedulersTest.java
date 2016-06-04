bf5c11d4d900284d3ec4b1481f1a9e8ef9db6530
Zac Sweers <pandanomic@gmail.com>
2016-06-04 00:55:27 -0700

    Address comments

--- rxandroid/src/test/java/rx/android/schedulers/ResetSchedulersTest.java
+++ rxandroid/src/test/java/rx/android/schedulers/ResetSchedulersTest.java
@@ -1,5 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package rx.android.schedulers;
-
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +23,7 @@ import rx.android.plugins.RxAndroidPlugins;
 import rx.android.plugins.RxAndroidSchedulersHook;
 import rx.schedulers.TestScheduler;
 
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
@@ -30,7 +43,7 @@ public final class ResetSchedulersTest {
         });
         AndroidSchedulers.reset();
 
-        assertTrue(AndroidSchedulers.mainThread().equals(testScheduler));
+        assertSame(AndroidSchedulers.mainThread(), testScheduler);
 
         RxAndroidPlugins.getInstance().reset();
         RxAndroidPlugins.getInstance().registerSchedulersHook(RxAndroidSchedulersHook.getDefaultInstance());
